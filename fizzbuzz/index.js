e8e27f46a90154bb3d1430b582cdec45c1e0d6f1
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-02-12 13:09:00 -0700

    updated index.js

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,7 +1,30 @@
+'use strict';
+
 function fizzBuzz() {
-    $(.js-results).click( event => {
-        
-    })
+  $('#number-chooser').submit( event => {
+    event.preventDefault();
+    $('js-results').empty();
+    const num = parseInt($(event.currentTarget)
+      .find('input[name="number-choice"]').val());
+    const array = [];
+    for( let i = 1; i <= num; i++ ) {
+      
+      if( i % 3 === 0 & i % 5 === 0) {
+        array.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      } 
+      else if( i % 3 === 0 ) {
+        array.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>'));
+      }
+      else if( i % 5 === 0 ) {
+        array.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      }
+    }
+    $('js-results').append(array);
+
+    
+
+
+  });
 }
 
 fizzBuzz();
