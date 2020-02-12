0c45f9b64ec7e3e36416074bd2b0f3ec5bdf4027
Agatha North <rouborn@gmail.com>
2020-02-12 10:57:07 -0800

    cat assignment

--- cat/index.js
+++ cat/index.js
@@ -1,5 +1,11 @@
 function handleThumbnailClicks(){
-    console.log("in fn")
+    $('.thumbnail').click(event => {
+        const thumbSrc = $(event.currentTarget).find('img').attr('src');
+        const thumbAlt = $(event.currentTarget).find('img').attr('alt');
+
+        $('.hero img').attr('src', thumbSrc);
+        $('.hero img').attr('alt', thumbAlt);
+    });
 }
 
 
