ce90dd262b19d62d41cc863b840e1e07750d1172
Agatha North <rouborn@gmail.com>
2020-02-12 12:22:23 -0800

    final

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,16 +1,14 @@
 'use strict';
 
-function fizzBuzz() {
+$(function() {
   $('#number-chooser').submit( event => {
     event.preventDefault();
     $('js-results').empty();
-    const num = parseInt($(event.currentTarget)
-      .find('input[name="number-choice"]').val());
+    const num = parseInt($(event.currentTarget).find('input[name="number-choice"]').val());
     const array = [];
     for( let i = 1; i <= num; i++ ) {
-      
-      if( i % 3 === 0 & i % 5 === 0) {
-        array.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      if( i % 15 === 0) {
+        array.push($('<div class="fizz-buzz-item buzz"><span>fizzbuzz</span></div>'));
       } 
       else if( i % 3 === 0 ) {
         array.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>'));
@@ -18,13 +16,10 @@ function fizzBuzz() {
       else if( i % 5 === 0 ) {
         array.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
       }
+      else {
+          array.push($(`<div class="fizz-buzz-item"><span>${i}</span></div>`));
+      }
     }
-    $('js-results').append(array);
-
-    
-
-
+    $('.js-results').append(array);
   });
-}
-
-fizzBuzz();
+});
